d41cd12e6ce8fae5665aa024a0384265d726598f
Matheus <matheusguimaraes@cjr.org.br>
2020-04-16 13:47:48 -0300

    aula_2/Prepara estrutura do projeto

---
+++ app/javascript/app.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+
+const App = () => {
+	return <Fragment>Hello App</Fragment>;
+};
+
+export default App;
--- app/javascript/packs/toyfy.jsx
+++ app/javascript/packs/toyfy.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from '../app';
 
 document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(<div>Hello Toyfy</div>, document.getElementById('root'));
+	ReactDOM.render(<App />, document.getElementById('root'));
 });
