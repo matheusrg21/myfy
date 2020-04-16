ea2825b16a30f375b64aea80cec69b7cc1efe9bb
Matheus <matheusguimaraes@cjr.org.br>
2020-04-16 13:33:49 -0300

    Adiciona React no projeto

---
+++ app/javascript/packs/toyfy.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+	ReactDOM.render(<div>Hello Toyfy</div>, document.getElementById('root'));
+});
